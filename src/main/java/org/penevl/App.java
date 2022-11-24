ecddc816e10a4229672143017d35853f49d20d89
penevl <103186686+penevl@users.noreply.github.com>
2022-11-24 22:06:30 +0200

    Airline ticket price calculator

--- src/main/java/org/penevl/App.java
+++ src/main/java/org/penevl/App.java
@@ -1,9 +1,26 @@
 package org.penevl;
 
+import java.util.Scanner;
+
 public class App 
 {
+
     public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    {       
+
+        Scanner sc = new Scanner(System.in);
+
+        int input = sc.nextInt();
+
+        if (input == 1 || input == 2){
+            System.out.println("Ticket price is: " + 160 * (70 / 100));
+        }else if (input == 3 || input == 4){
+            System.out.println("Ticket price is: " + 160 * (75 / 100));
+        }else{
+            System.out.println("Ticket price is: " + 160);
+        }
+
+        sc.close();
+
     }
 }
