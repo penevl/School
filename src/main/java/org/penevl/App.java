c895a83a82c66185b438acc4108461fe01c6ae85
ElDuko <penevl@protonmail.com>
2022-11-25 12:16:57 +0200

    Removed intellij config settings

--- src/main/java/org/penevl/App.java
+++ src/main/java/org/penevl/App.java
@@ -1,26 +1,13 @@
 package org.penevl;
 
-import java.util.Scanner;
-
 public class App 
 {
 
     public static void main( String[] args )
     {       
 
-        Scanner sc = new Scanner(System.in);
-
-        int input = sc.nextInt();
-
-        if (input == 1 || input == 2){
-            System.out.println("Ticket price is: " + 160 * (70 / 100));
-        }else if (input == 3 || input == 4){
-            System.out.println("Ticket price is: " + 160 * (75 / 100));
-        }else{
-            System.out.println("Ticket price is: " + 160);
-        }
-
-        sc.close();
+        for (int i=1;i<=20;i++)
+            System.out.print(i + " ");
 
     }
 }
