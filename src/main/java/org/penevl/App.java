b180139b542a566b96e93b0f0114dc94555d79fa
elduko <penevl@protonmail.com>
2022-12-01 23:44:05 +0200

    Added missing features (#1)
    Co-authored-by: penevl <103186686+penevl@users.noreply.github.com>
    Reviewed-on: http://"git-gitea.ix-git.svc.cluster.local"/elduko/School/pulls/1

--- src/main/java/org/penevl/App.java
+++ src/main/java/org/penevl/App.java
@@ -12,6 +12,7 @@ public class App
         Scanner sc = new Scanner(System.in);
 
         int input;
+        int count = 0;
         int result = 1;
 
         do{
@@ -24,11 +25,14 @@ public class App
 
         for (int i=1;i<input;i++){
             if(i % 5 == 0){
+                count++;
                 result = result * i;
             }
         }
 
-        System.out.println(result);
+        System.out.println("Result is: " + result);
+        System.out.println("Number of numbers divisible by 5 is: " + count);
+        // Не е възможно да няма числа които се делят на 5 защото се проверява дали числото въведено е по-малко от 5
 
         sc.close();
 
