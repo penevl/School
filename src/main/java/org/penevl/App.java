aa80308298dddaee4e8b51942819cc017a964286
penevl <103186686+penevl@users.noreply.github.com>
2022-12-07 19:06:57 +0200

    Fixed merge errors

--- src/main/java/org/penevl/App.java
+++ src/main/java/org/penevl/App.java
@@ -11,34 +11,6 @@ public class App
 
         System.out.println("Enter 2 numbers the second one higher than the first.");
         Scanner sc = new Scanner(System.in);
-        int m,n;
-        int count = 0;
-        int sum = 0;
-
-<<<<<<< HEAD
-        do{
-
-            n = sc.nextInt();
-            if (n < 100 && n > 999)
-                System.out.println("You need to enter a 3 digit number");
-
-        }while(n < 100 && n > 999);
-
-
-        do{
-
-            m = sc.nextInt();
-            if (m < 100 && m > 999)
-                System.out.println("You need to enter a 3 digit number");
-
-        }while(m < 100 && m > 999);
-
-        for (int i=n;i<m;i++){
-            if (i/10%10 == 3) {
-                count++;
-                sum = sum + i;
-            }
-        }
 
         int n,m;
         int count = 0;
@@ -58,7 +30,6 @@ public class App
         System.out.print("Avg. is: " + avarage);
 
         sc.close();
->>>>>>> b180139b542a566b96e93b0f0114dc94555d79fa
 
     }
 }
