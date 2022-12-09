9b4ef476ceeeb7097ec786ca380df5ec913ea98c
ElDuko <penevl@protonmail.com>
2022-12-09 12:31:09 +0200

    Infinite counter

--- src/main/java/org/penevl/App.java
+++ src/main/java/org/penevl/App.java
@@ -9,29 +9,22 @@ public class App
     public static void main( String[] args )
     {       
 
-        System.out.println("Enter 2 numbers the second one higher than the first.");
+        System.out.println("Enter a number");
         Scanner sc = new Scanner(System.in);
 
-        int n,m;
-        int count = 0;
-        int result = 0;
+        int n;
+        double sum = 0;
 
         n = sc.nextInt();
-        m = sc.nextInt();
 
-        for (int i = n; i <= m; i++) {
-            if (i % 3 == 0 || i % 7 == 0){
-                result += i;
-                count++;
-            }
+        for (int i = 1; i <= n; i++) {
+
+            sum = sum + 1 + 1.0 / (i*i);
+
         }
 
-        float avarage = result / count;
-        if (avarage == 0) {
-            System.out.println("No numbers matched the required conditions");
-        }else{
-            System.out.print("Avg. is: " + avarage);
-        }
+        System.out.println(sum);
+
         sc.close();
 
     }
